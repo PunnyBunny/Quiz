872b554aec92fbccd98831a441ef11f0534387fd
Sunny <sunnycheungabc@gmail.com>
2020-12-07 18:51:43 +0800

    Put Scaffold in summary.dart

--- lib/src/summary.dart
+++ lib/src/summary.dart
@@ -12,31 +12,33 @@ class _SummaryPageState extends State<SummaryPage> {
 
     return WillPopScope(
       onWillPop: () async => false,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              finalScore,
-              style: TextStyle(fontSize: 40.0, color: Colors.white),
-            ),
-            Padding(padding: EdgeInsets.all(30.0)),
-            MaterialButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                finalScore,
+                style: TextStyle(fontSize: 40.0, color: Colors.white),
               ),
-              color: Colors.blue,
-              onPressed: () => Navigator.pushNamedAndRemoveUntil(
-                  context, '/', ModalRoute.withName('/info_form')),
-              child: Text(
-                "離開",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
+              Padding(padding: EdgeInsets.all(30.0)),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                color: Colors.blue,
+                onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                    context, '/', ModalRoute.withName('/info_form')),
+                child: Text(
+                  "離開",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
