45c6b555a9a0a1924c441d609e54c97aadb19b5f
Sunny <sunnycheungabc@gmail.com>
2020-12-11 15:57:39 +0800

    Enforce portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/info_form.dart';
 import 'src/quiz_home.dart';
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: '測試',
