33dd7e1502cd0c975f55735b8ae7c5d0471b5011
Sunny <sunnycheungabc@gmail.com>
2020-12-11 12:49:41 +0800

    Align text and optimise code

--- lib/src/basic_quiz.dart
+++ lib/src/basic_quiz.dart
@@ -44,89 +44,18 @@ class _QuizState extends State<Quiz> {
       onWillPop: () async => false,
       child: Scaffold(
         appBar: AppBar(
-          title: Container(
-            alignment: Alignment.centerRight,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  '第 ${_questionNumber + 1}/${widget.length} 題',
-                  style: TextStyle(fontSize: 22.0),
-                ),
-              ],
-            ),
+          title: Text(
+            '第 ${_questionNumber + 1}/${widget.length} 題',
+            style: TextStyle(fontSize: 22.0),
           ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    content: Text(
-                      '確認退出？',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.popUntil(
-                              context, ModalRoute.withName('/'));
-                        },
-                        child: Text('退出'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text('取消'),
-                      )
-                    ],
-                  );
-                },
-              );
-            },
-          ),
+          leading: _backButton(),
         ),
         body: ListView(
           children: [
             Column(
               children: <Widget>[
-                    // audio
-                    widget.audios == null
-                        ? Container()
-                        : Padding(
-                            padding: EdgeInsets.all(2.0),
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(5.0)),
-                                side:
-                                    BorderSide(color: Colors.white, width: 1.0),
-                                minimumSize: Size(150.0, 25.0),
-                                primary: Colors.lightBlue,
-                              ),
-                              child: Text('播放聲音'),
-                              onPressed: () {
-                                _assetsAudioPlayer.open(
-                                  Audio(
-                                    'assets/audios/${widget.audios[_questionNumber]}.mp3',
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-
-                    // question
-                    Padding(
-                      padding: EdgeInsets.all(2.0),
-                      child: Text(
-                        widget.questions[_questionNumber],
-                        style: TextStyle(
-                          fontSize: 25.0,
-                        ),
-                      ),
-                    ),
+                    _audioButton(),
+                    _question(),
                   ] +
                   _choiceButtons() +
                   [
@@ -135,116 +64,88 @@ class _QuizState extends State<Quiz> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          // prev question button
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                primary: _questionNumber == 0
-                                    ? Colors.blueGrey
-                                    : Colors.lightBlue,
-                                shape: CircleBorder(),
-                                minimumSize: Size(50.0, 50.0)),
-                            child: Icon(Icons.arrow_back_rounded,
-                                color: Colors.white, size: 50),
-                            onPressed: () {
-                              if (_questionNumber > 0)
-                                setState(() {
-                                  _questionNumber--;
-                                });
-                            },
-                          ),
-
-                          // submit button
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: _noOfQuestionsFilled == widget.length
-                                  ? Colors.green
-                                  : Colors.blueGrey,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16)),
-                              minimumSize: Size(100.0, 50.0),
-                            ),
-                            child: Text('遞交'),
-                            onPressed: () {
-                              if (_noOfQuestionsFilled == widget.length) {
-                                int score = 0;
-                                for (int i = 0; i < widget.length; ++i) {
-                                  if (_choices[i] == widget.correctAnswers[i]) {
-                                    score++;
-                                  }
-                                }
-                                Navigator.pushNamed(
-                                  context,
-                                  '/summary',
-                                  arguments: UserResult(
-                                    currentUserInfo.name,
-                                    currentUserInfo.dateOfBirth,
-                                    currentUserInfo.gender,
-                                    score,
-                                    widget.title,
-                                    widget.length,
-                                  ),
-                                );
-                              } else {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      title: Text('注意'),
-                                      content: Text(
-                                        '請先完成所有題目',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline1,
-                                      ),
-                                      actions: [
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Text('好'),
-                                        )
-                                      ],
-                                    );
-                                  },
-                                );
-                              }
-                            },
-                          ),
-
-                          // next question button
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                primary: _questionNumber == widget.length - 1
-                                    ? Colors.blueGrey
-                                    : Colors.lightBlue,
-                                shape: CircleBorder(),
-                                minimumSize: Size(50.0, 50.0)),
-                            child: Icon(Icons.arrow_forward_rounded,
-                                color: Colors.white, size: 50),
-                            onPressed: () {
-                              if (_questionNumber + 1 < widget.length)
-                                setState(() {
-                                  _questionNumber++;
-                                });
-                            },
-                          ),
+                          _prevQuestionButton(),
+                          _submitButton(),
+                          _nextQuestionButton(),
                         ],
                       ),
                     ),
-
-                    // image
-                    widget.images == null
-                        ? Container()
-                        : Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: Image.asset(
-                              'assets/images/${widget.images[_questionNumber]}',
-                            ),
-                          ),
+                    _image(),
                   ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _backButton() {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(
+                '確認退出？',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.popUntil(context, ModalRoute.withName('/'));
+                  },
+                  child: Text('退出'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('取消'),
+                )
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _audioButton() {
+    return widget.audios == null
+        ? Container()
+        : Padding(
+            padding: EdgeInsets.all(2.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
+                side: BorderSide(color: Colors.white, width: 1.0),
+                minimumSize: Size(150.0, 25.0),
+                primary: Colors.lightBlue,
+              ),
+              child: Text('播放聲音'),
+              onPressed: () {
+                _assetsAudioPlayer.open(
+                  Audio(
+                    'assets/audios/${widget.audios[_questionNumber]}.mp3',
+                  ),
+                );
+              },
+            ),
+          );
+  }
+
+  Widget _question() {
+    return Padding(
+      padding: EdgeInsets.all(2.0),
+      child: Text(
+        widget.questions[_questionNumber],
+        style: TextStyle(
+          fontSize: 25.0,
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
@@ -284,5 +185,106 @@ class _QuizState extends State<Quiz> {
           ),
         )
         .toList(); // choices
+  }
+
+  Widget _prevQuestionButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          primary: _questionNumber == 0 ? Colors.blueGrey : Colors.lightBlue,
+          shape: CircleBorder(),
+          minimumSize: Size(50.0, 50.0)),
+      child: Icon(Icons.arrow_back_rounded, color: Colors.white, size: 50),
+      onPressed: () {
+        if (_questionNumber > 0)
+          setState(() {
+            _questionNumber--;
+          });
+      },
+    );
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: _noOfQuestionsFilled == widget.length
+            ? Colors.green
+            : Colors.blueGrey,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        minimumSize: Size(100.0, 50.0),
+      ),
+      child: Text('遞交'),
+      onPressed: () {
+        if (_noOfQuestionsFilled == widget.length) {
+          int score = 0;
+          for (int i = 0; i < widget.length; ++i) {
+            if (_choices[i] == widget.correctAnswers[i]) {
+              score++;
+            }
+          }
+          Navigator.pushNamed(
+            context,
+            '/summary',
+            arguments: UserResult(
+              currentUserInfo.name,
+              currentUserInfo.dateOfBirth,
+              currentUserInfo.gender,
+              score,
+              widget.title,
+              widget.length,
+            ),
+          );
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('注意'),
+                content: Text(
+                  '請先完成所有題目',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('好'),
+                  )
+                ],
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Widget _nextQuestionButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          primary: _questionNumber == widget.length - 1
+              ? Colors.blueGrey
+              : Colors.lightBlue,
+          shape: CircleBorder(),
+          minimumSize: Size(50.0, 50.0)),
+      child: Icon(Icons.arrow_forward_rounded, color: Colors.white, size: 50),
+      onPressed: () {
+        if (_questionNumber + 1 < widget.length)
+          setState(() {
+            _questionNumber++;
+          });
+      },
+    );
+  }
+
+  Widget _image() {
+    return widget.images == null
+        ? Container()
+        : Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Image.asset(
+              'assets/images/${widget.images[_questionNumber]}',
+            ),
+          );
   }
 }
