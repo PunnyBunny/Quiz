f8984fbfd33b7e800ccd3c7696c0e26021ba1e3f
Sunny <sunnycheungabc@gmail.com>
2020-12-11 12:53:30 +0800

    Align text and optimise code

--- lib/src/info_form.dart
+++ lib/src/info_form.dart
@@ -35,124 +35,136 @@ class _InformationFormState extends State<InformationForm> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              autofocus: false,
-              onChanged: (name) {
-                setState(() {
-                  _userName = name;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: '姓名',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ), // name
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(2.0)),
-                side: BorderSide(color: Colors.white, width: 1.0),
-                minimumSize: Size(double.infinity, 50.0),
-                primary: Colors.transparent,
-              ),
-              child: Text('出生日期: ' + dateFormatter.format(_userDateOfBirth)),
-              onPressed: () async {
-                final DateTime picked = await showDatePicker(
-                  context: context,
-                  initialDate: _userDateOfBirth,
-                  firstDate: DateTime(1960),
-                  lastDate: DateTime.now(),
-                );
-                if (picked != null && picked != _userDateOfBirth) {
-                  setState(() {
-                    _userDateOfBirth = picked;
-                  });
-                }
-              },
-            ),
-          ), // date of birth
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: DropdownButton(
-              value: _userGender.isEmpty ? null : _userGender,
-              hint: Text('性別'),
-              items: [
-                DropdownMenuItem(value: 'm', child: Text('男')),
-                DropdownMenuItem(value: 'f', child: Text('女')),
-              ],
-              onChanged: (value) {
-                setState(() {
-                  _userGender = value;
-                });
-              },
-            ),
-          ), // gender
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2.0)),
-              side: BorderSide(color: Colors.white, width: 1.0),
-              primary: Colors.green,
-            ),
-            child: Text('遞交'),
-            onPressed: () async {
-              setState(() {
-                _userNameWarning = _userName.isEmpty;
-                _userDateOfBirthWarning =
-                    dateFormatter.format(_userDateOfBirth) ==
-                        dateFormatter.format(DateTime.now());
-                _userGenderWarning = _userGender.isEmpty;
-              });
-              if (!_userNameWarning &&
-                  !_userDateOfBirthWarning &&
-                  !_userGenderWarning) {
-                // all information is correctly filed
-                currentUserInfo =
-                    UserInfo(_userName, _userDateOfBirth, _userGender);
-
-                if (!_loadedJson) {
-                  String json = await rootBundle.loadString('assets/data.json');
-                  List<dynamic> loaded = jsonDecode(json);
-                  loaded.forEach((data) {
-                    quizzes.add(Quiz.fromJson(data));
-                  });
-                  _loadedJson = true;
-                }
-                Navigator.pushNamed(context, '/');
-              } else {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('注意'),
-                      content: Text(
-                        '${_userNameWarning ? '請填寫姓名\n' : ''}'
-                        '${_userDateOfBirthWarning ? '請填寫出生日期\n' : ''}'
-                        '${_userGenderWarning ? '請填寫性別\n' : ''}',
-                        style: Theme.of(context).textTheme.headline1,
-                      ),
-                      actions: [
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text('好'),
-                        )
-                      ],
-                    );
-                  },
-                );
-              }
-            },
-          ),
-          Padding(padding: EdgeInsets.all(18.0)),
+          _nameField(),
+          _dateOfBirthField(),
+          _genderField(),
+          _submitButton(),
         ],
       ),
+    );
+  }
+
+  Widget _nameField() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: TextField(
+        autofocus: false,
+        onChanged: (name) {
+          setState(() {
+            _userName = name;
+          });
+        },
+        decoration: InputDecoration(
+          hintText: '姓名',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget _dateOfBirthField() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(2.0)),
+          side: BorderSide(color: Colors.white, width: 1.0),
+          minimumSize: Size(double.infinity, 50.0),
+          primary: Colors.transparent,
+        ),
+        child: Text('出生日期: ' + dateFormatter.format(_userDateOfBirth)),
+        onPressed: () async {
+          final DateTime picked = await showDatePicker(
+            context: context,
+            initialDate: _userDateOfBirth,
+            firstDate: DateTime(1960),
+            lastDate: DateTime.now(),
+          );
+          if (picked != null && picked != _userDateOfBirth) {
+            setState(() {
+              _userDateOfBirth = picked;
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _genderField() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: DropdownButton(
+        value: _userGender.isEmpty ? null : _userGender,
+        hint: Text('性別'),
+        items: [
+          DropdownMenuItem(value: 'm', child: Text('男')),
+          DropdownMenuItem(value: 'f', child: Text('女')),
+        ],
+        onChanged: (value) {
+          setState(() {
+            _userGender = value;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(2.0)),
+        side: BorderSide(color: Colors.white, width: 1.0),
+        primary: Colors.green,
+      ),
+      child: Text('遞交'),
+      onPressed: () async {
+        setState(() {
+          _userNameWarning = _userName.isEmpty;
+          _userDateOfBirthWarning = dateFormatter.format(_userDateOfBirth) ==
+              dateFormatter.format(DateTime.now());
+          _userGenderWarning = _userGender.isEmpty;
+        });
+        if (!_userNameWarning &&
+            !_userDateOfBirthWarning &&
+            !_userGenderWarning) {
+          // all information is correctly filed
+          currentUserInfo = UserInfo(_userName, _userDateOfBirth, _userGender);
+
+          if (!_loadedJson) {
+            String json = await rootBundle.loadString('assets/data.json');
+            List<dynamic> loaded = jsonDecode(json);
+            loaded.forEach((data) {
+              quizzes.add(Quiz.fromJson(data));
+            });
+            _loadedJson = true;
+          }
+          Navigator.pushNamed(context, '/');
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('注意'),
+                content: Text(
+                  '${_userNameWarning ? '請填寫姓名\n' : ''}'
+                  '${_userDateOfBirthWarning ? '請填寫出生日期\n' : ''}'
+                  '${_userGenderWarning ? '請填寫性別\n' : ''}',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('好'),
+                  )
+                ],
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
