487afc46d835d822ed64e3f1aa8a36ea039a2d48
Sunny <sunnycheungabc@gmail.com>
2020-12-23 15:05:40 +0800

    Make class immutable

--- lib/src/instructions.dart
+++ lib/src/instructions.dart
@@ -2,48 +2,46 @@ import 'package:flutter/material.dart';
 
 void pushInstructionPage(BuildContext context, List<Widget> children) {
   Navigator.push(context, MaterialPageRoute(builder: (_) {
-    return InstructionPage(children);
+    return InstructionPage(children: children);
   }));
 }
 
 class InstructionPage extends StatelessWidget {
-  List<Widget> children;
-  bool _initiated = false;
+  final List<Widget> children;
 
-  InstructionPage(this.children);
+  const InstructionPage({Key key, this.children}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (!_initiated) {
-      _init(context);
-    }
+    final padding = Padding(padding: EdgeInsets.all(20.0));
 
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: children,
+          children: [_title(context), padding] +
+              children +
+              [padding, _backButton(context)],
         ),
       ),
     );
   }
 
-  void _init(BuildContext context) {
-    final title = Text(
+  Widget _title(BuildContext context) {
+    return Text(
       '香港中學生粵語語義能力測試',
       style: Theme.of(context).textTheme.headline5,
       textAlign: TextAlign.center,
     );
-    final back = ElevatedButton(
+  }
+
+  Widget _backButton(BuildContext context) {
+    return ElevatedButton(
       style: ElevatedButton.styleFrom(primary: Colors.blue),
       child: Text('知道'),
       onPressed: () {
         Navigator.pop(context);
       },
     );
-    final padding = Padding(padding: EdgeInsets.all(20.0));
-
-    children = <Widget>[title, padding] + children + <Widget>[padding, back];
-    _initiated = true;
   }
 }
